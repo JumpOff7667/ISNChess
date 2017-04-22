81e7a63fc5edaf719789bfd2fcf57302ee0ffa1d
JumpOff7667 <hackingontop@gmail.com>
2017-04-22 22:10:37 +0200

    Adding all files to repository

---
+++ chessboard - rectetcoor.py
@@ -0,0 +1,62 @@
+import sys, pygame
+pygame.init()
+
+#Parametrages
+size = width, height = 512, 512
+sizepion = width, height = 64, 64
+sizecarre = width, height = 64, 64
+lignes, colonnes = 8, 8
+black = 0, 0, 0
+
+#Variables
+countlist = {}
+num = 0
+case = [[0] * colonnes for _ in range(lignes)]
+tours = []
+selection = 0
+stopsel = 0
+
+#Definir la taille de l'ecran
+screen = pygame.display.set_mode(size)
+
+#Charger l'image du damier et son rectangle
+damier = pygame.image.load("Damier.gif")
+damierect = damier.get_rect()
+
+#Charger tour et son rectangle (avec redimension)
+tour_n = pygame.image.load("rook.png")
+tour_n = pygame.transform.scale(tour_n, (sizepion))
+tourN = tour_n.get_rect()
+tours.append(tourN)
+
+#Carres du damier
+for i in range (0,8):
+    for j in range (0, 8):
+        case[i][j] = pygame.Rect(i*64, j*64, 64, 64)
+        
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            r1, r2 = int(x/64), int(y/64)
+            if case[r1][r2].colliderect(tourN):
+                selection = tourect_n
+                print("ok for selection")
+            if selection in tours:
+                r3, r4 = int(x/64), int(y/64)
+                if not case[r3][r4].colliderect(selection):  
+                    selection.clamp_ip(case[r3][r4])
+                    selection = 0
+                    print("ok for clamp")
+                        
+                        
+                    
+                
+    screen.blit(damier, damierect)
+    screen.blit(tour_n, tourN)
+    pygame.display.flip()
+
+        
